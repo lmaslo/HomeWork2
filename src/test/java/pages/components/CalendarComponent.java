c6e32f71bcf9e7c0bf17aa2d37fae920e0caf508
lmaslo <PerepechkinaEV@yandex.ru>
2022-02-14 15:34:16 +0300

    add calendar

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -8,6 +8,7 @@ public class CalendarComponent {
     public void setDate (String year, String month, String day ){
         $(".react-datepicker__year-select").selectOptionByValue(year);
         $(".react-datepicker__month-select").selectOptionByValue(month);
+       // $(".react-datepicker__day--010:not(.react-datepicker__day--outside-month)").click();
         $(byText(day)).click();
     }
 
