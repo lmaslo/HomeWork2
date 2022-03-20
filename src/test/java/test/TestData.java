8b65edfbff32f1f9052b1b0759418ca41bd2d2f7
Lena Masloboishchikova <perepechkinaEV@yandex.ru>
2022-03-20 14:47:20 +0300

    Faker from CheckForm to TestData add Attach.java

--- src/test/java/test/TestData.java
+++ src/test/java/test/TestData.java
@@ -15,7 +15,7 @@ public class TestData {
 
 
     public static String email = "lMaslo@mail.ru",
-            number = "8999932145",
+            number = "8999932146",
             firstName = getRandomString(8);
 
 }
