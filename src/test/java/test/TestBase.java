cf569f215d94b121909ad6c77789401235e2d55d
Lena Masloboishchikova <perepechkinaEV@yandex.ru>
2022-03-24 16:00:30 +0300

    add properties

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -11,15 +11,20 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
+
+    static String remoteBrowser = System.getProperty("remoteBrowser", "selenoid.autotests.cloud/wd/hub");
+    static String remoteBrowserUser = System.getProperty("remoteBrowserUser", "user1");
+    static String remoteBrowserPassword = System.getProperty("remoteBrowserPassword", "1234");
+
+
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
 
         //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        Configuration.remote = System.getProperty("browser");
+        Configuration.remote = "https://" + remoteBrowserUser + ":" + remoteBrowserPassword + "@" + remoteBrowser;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
